2cc465c43890c7d5a61c7e12c8f11715b789f43d
Phani2425 <mohantyphanibhusan@gmail.com>
2025-03-09 00:19:18 +0530

    Add Web Search and Image Search functionality with routing and CORS support

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,24 @@
 import express from 'express';
 import http from 'http';
 import dotenv from 'dotenv';
+import router from './routes';
+import cors from 'cors';
 dotenv.config();
 
 const app = express();
 const server = http.createServer(app);
+
+const corsOptions = {
+    origin:'*',
+};
+
+app.use(cors(corsOptions));
+app.use(express.json());
+app.use('/api',router);
+
+app.get('/', (_,resp) => {
+    resp.send(`server is up and running on port :- ${PORT}`)
+})
 
 const PORT = process.env.PORT || 5000;
 server.listen(PORT,() => {
