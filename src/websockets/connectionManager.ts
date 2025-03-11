946406b6af8d0e498ca83e38051099cad5c183bd
Phani2425 <mohantyphanibhusan@gmail.com>
2025-03-12 03:17:00 +0530

    Initialize frontend with Next.js, Tailwind CSS, and basic configuration files

--- src/websockets/connectionManager.ts
+++ src/websockets/connectionManager.ts
@@ -4,8 +4,7 @@ import { handleMessage } from "./messageHandler"
 
 export const handleConnection = (ws:WebSocket) => {
   ws.on('message',
-    async (message) => await handleMessage(message.toString(), ws,"fkjbfbf");
-   );
+    async (message) => await handleMessage(message.toString(), ws,"fkjbfbf"))
 
    ws.on("close", () => console.log("connection closed"));
 };
